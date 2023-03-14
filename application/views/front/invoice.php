b048caceb277c7df7deaddbca470cc61adae5dc0
keshridivya <dkeshari094@gmail.com>
2023-03-14 17:37:02 +0530

    invoice

--- application/views/front/invoice.php
+++ application/views/front/invoice.php
@@ -30,7 +30,7 @@
 		width: 100%;
 		height: 100%;
 		padding: 50px 0;
-        overflow-x:scroll;
+		overflow-x: scroll;
 	}
 
 	#invoice {
@@ -45,7 +45,8 @@
 	}
 
 	#invoice-top {
-		border-bottom: 2px solid    var(--var-green);;
+		border-bottom: 2px solid var(--var-green);
+		;
 	}
 
 	#invoice-mid {
@@ -110,8 +111,9 @@
 	}
 
 	.cta-group .btn-primary {
-		background:    var(--var-green);;
-        display: inline-block;
+		background: var(--var-green);
+		;
+		display: inline-block;
 		padding: 7px;
 		border-radius: 4px;
 		background: rgb(196, 57, 10);
@@ -139,9 +141,9 @@
 
 	.tabletitle th {
 		border: 2px solid #fff;
-        background:var(--var-green);
+		background: var(--var-green);
 		text-align: right;
-        color:#fff;
+		color: #fff;
 	}
 
 	.tabletitle th:nth-child(2) {
@@ -306,36 +308,41 @@
 				</p>
 			</div> -->
 
-            <div class="clearfix">
-            <div class="col-left">
-                <div class="logo"><img src="<?php echo base_url();?>assets/images/logo/header.png" alt="OTG CARES" /></div>
-                <div class="clientinfo">
-                    <h2 id="supplier">OTGcares</h2>
-                    <p><span id="address">S-53, Haware Fantasia Business park, <br> Vashi Navi Mumbai Thane Maharashtra 400708</span><br>
-                    <span><b>Phone : </b> <span>9076020306</span></span><br>
-            <span><b>Email : </b> <span>support@otgcares.com</span></span></p>
-                   
-                </div>
-            </div>
-            <div class="col-right">
-            <h2 id="supplier">INVOICE</h2>
-                <table class="table">
-                    <tbody>
-                        <tr>
-                            <td><span>Invoice Number</span><label id="invoice_no" class='float-right'>INV-<?= $invoice_create[0]['request_id_value'] ?></label></td>
-                        </tr>
-                        <tr>
-                            <td><span>Order Number</span><label id="order-number"  class='float-right'><?= $invoice_create[0]['request_id_value'] ?></label></td>
-                        </tr>
-                        <tr>
-                            <td><span>Invoice Date</span><label id="invoice_date" class='float-right'><?php  $input=$invoice_create[0]['created_date']; 
+			<div class="clearfix">
+				<div class="col-left">
+					<div class="logo"><img src="<?php echo base_url();?>assets/images/logo/header.png"
+							alt="OTG CARES" /></div>
+					<div class="clientinfo">
+						<h2 id="supplier">OTGcares</h2>
+						<p><span id="address">S-53, Haware Fantasia Business park, <br> Vashi Navi Mumbai Thane
+								Maharashtra 400708</span><br>
+							<span><b>Phone : </b> <span>9076020306</span></span><br>
+							<span><b>Email : </b> <span>support@otgcares.com</span></span></p>
+
+					</div>
+				</div>
+				<div class="col-right">
+					<h2 id="supplier">INVOICE</h2>
+					<table class="table">
+						<tbody>
+							<tr>
+								<td><span>Invoice Number</span><label id="invoice_no"
+										class='float-right'>INV-<?= $invoice_create[0]['request_id_value'] ?></label>
+								</td>
+							</tr>
+							<tr>
+								<td><span>Order Number</span><label id="order-number"
+										class='float-right'><?= $invoice_create[0]['request_id_value'] ?></label></td>
+							</tr>
+							<tr>
+								<td><span>Invoice Date</span><label id="invoice_date" class='float-right'><?php  $input=$invoice_create[0]['created_date']; 
 										$date=strtotime($input);
 										echo date('d  M  Y',$date) ?></label></td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
-        </div>    
+							</tr>
+						</tbody>
+					</table>
+				</div>
+			</div>
 			<!--End Title-->
 		</div>
 
@@ -343,7 +350,8 @@
 		<div id="invoice-mid">
 			<div id="message">
 				<h2>Bill To,</h2>
-				<p><?= $invoice_create[0]['name'] ?> <span id="supplier_name"></span><br>+91 <?= $invoice_create[0]['contact'] ?></p>
+				<p><?= $invoice_create[0]['name'] ?> <span id="supplier_name"></span><br>+91
+					<?= $invoice_create[0]['contact'] ?></p>
 			</div>
 			<!-- <div class="cta-group mobile-btn-group">
 				<a href="javascript:void(0);" class="btn-primary">Approve</a>
@@ -404,7 +412,8 @@
 						</td>
 						<td data-label="Quantity" class="tableitem"><?= $invoice_create[0]['quantity'] ?></td>
 						<td data-label="Unit Price" class="tableitem"><?= $invoice_create[0]['total_amount'] ?></td>
-						<td data-label="Taxable Amount" class="tableitem" id="cBalance"><?= $invoice_create[0]['total_amount'] ?></td>
+						<td data-label="Taxable Amount" class="tableitem" id="cBalance">
+							<?= $invoice_create[0]['total_amount'] ?></td>
 						<td data-label="Tax Code" class="tableitem" id='chDiscount'>20</td>
 						<td data-label="%" class="tableitem"><?= '0.00' ?></td>
 						<td data-label="Tax Amount" class="tableitem" id='result'></td>
@@ -421,41 +430,48 @@
 						<td data-label="Tax Amount" class="tableitem">9.32</td>
 					</tr> -->
 					<tr class="list-item">
-						<td colspan="6" style="border-left:2px solid #dee2e6;text-transform:capitalize" class='words text-left'></td>
-                        <td data-label="Grand Total" class="tableitem">Subtotal</td>
-						<td data-label="Grand Total" class="tableitem numberword" id='result'><?= $invoice_create[0]['total_amount'] ?></td>
+						<td colspan="6" style="border-left:2px solid #dee2e6;text-transform:capitalize"
+							class='words text-left'></td>
+						<td data-label="Grand Total" class="tableitem">Subtotal</td>
+						<td data-label="Grand Total" class="tableitem numberword" id='result'>
+							<?= $invoice_create[0]['total_amount'] ?></td>
 					</tr>
-                    <tr class="list-item">
+					<tr class="list-item">
 						<td colspan="6" style="border-left:2px solid #dee2e6" class=' text-left'> Total Quantity: 9</td>
-                        <td data-label="Grand Total" class="tableitem">Total Payable</td>
-						<td data-label="Grand Total" class="tableitem" id='result'><?= $invoice_create[0]['total_amount'] ?></td>
+						<td data-label="Grand Total" class="tableitem">Total Payable</td>
+						<td data-label="Grand Total" class="tableitem" id='result'>
+							<?= $invoice_create[0]['total_amount'] ?></td>
 					</tr>
-                    <tr class="list-item">
+					<tr class="list-item">
 						<td colspan="6" style="border-bottom: 2px solid #dee2e6;border-left:2px solid #dee2e6"></td>
-                        <td data-label="Grand Total" class="tableitem">Received</td>
-						<td data-label="Grand Total" class="tableitem" id='result'><?= $invoice_create[0]['total_amount'] ?></td>
+						<td data-label="Grand Total" class="tableitem">Received</td>
+						<td data-label="Grand Total" class="tableitem" id='result'>
+							<?= $invoice_create[0]['total_amount'] ?></td>
 					</tr>
 				</table>
 			</div>
 
-            <div id="table">
+			<div id="table">
 				<table class="table table-bordered ">
-						<tr class="">
-							<th>Notes <p>Thank You For Doing Business With Us</p></th>
-							<th >Bank Details</th>
-							<th ><span>Due Amount</span><span>
-                                0.00
-                            </span></th>
-							
-						</tr>
+					<tr class="">
+						<th>Notes <p>Thank You For Doing Business With Us</p>
+						</th>
+						<th>Bank Details</th>
+						<th><span>Due Amount</span><span>
+								0.00
+							</span></th>
+
+					</tr>
 
 					<tr class="">
-						<td colspan='2'><h2>Terms And Conditions</h2>
-                            <p> A.M.C Upto 1.5 Ton - 3 Jet Service + 1 Time Gas Recharge + Unlimited Repair Total 3 Basic AMC And 2 Service AMC Is Included In Your Contract.</p>
-</td>
-						<td  class="">
-                            heelo
-                            <p> For OTGCares</p>
+						<td colspan='2'>
+							<h2>Terms And Conditions</h2>
+							<p> A.M.C Upto 1.5 Ton - 3 Jet Service + 1 Time Gas Recharge + Unlimited Repair Total 3
+								Basic AMC And 2 Service AMC Is Included In Your Contract.</p>
+						</td>
+						<td class="">
+							heelo
+							<p> For OTGCares</p>
 						</td>
 					</tr>
 
@@ -463,7 +479,7 @@
 			</div>
 			<!--End Table-->
 			<div class="cta-group">
-				<button  class="btn-primary invoice" >Download Invoice</button>
+				<button class="btn-primary invoice">Download Invoice</button>
 			</div>
 
 		</div>
@@ -472,58 +488,104 @@
 </div><!-- End Invoice Holder-->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.22/pdfmake.min.js"></script>
-<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/html2canvas/0.4.1/html2canvas.min.js"></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/html2canvas/0.4.1/html2canvas.min.js">
+</script>
 <script>
-    $(window).on("load", function () {
-    let numberword=$(".numberword").text();
-    $('.words').html(toWords(numberword),'rupees only');  
-});
+	$(window).on("load", function () {
+		let numberword = $(".numberword").text();
+		$('.words').html(toWords(numberword), 'rupees only');
+	});
 
-// American Numbering System
-let th = ['','thousand','million', 'billion','trillion'];
-// uncomment this line for English Number System
-// var th = ['','thousand','million', 'milliard','billion'];
+	// American Numbering System
+	let th = ['', 'thousand', 'million', 'billion', 'trillion'];
+	// uncomment this line for English Number System
+	// var th = ['','thousand','million', 'milliard','billion'];
 
-let dg = ['zero','one','two','three','four', 'five','six','seven','eight','nine']; var tn = ['ten','eleven','twelve','thirteen', 'fourteen','fifteen','sixteen', 'seventeen','eighteen','nineteen']; var tw = ['twenty','thirty','forty','fifty', 'sixty','seventy','eighty','ninety']; function toWords(s){s = s.toString(); s = s.replace(/[\, ]/g,''); if (s != parseFloat(s)) return 'not a number'; var x = s.indexOf('.'); if (x == -1) x = s.length; if (x > 15) return 'too big'; var n = s.split(''); var str = ''; var sk = 0; for (var i=0; i < x; i++) {if ((x-i)%3==2) {if (n[i] == '1') {str += tn[Number(n[i+1])] + ' '; i++; sk=1;} else if (n[i]!=0) {str += tw[n[i]-2] + ' ';sk=1;}} else if (n[i]!=0) {str += dg[n[i]] +' '; if ((x-i)%3==0) str += 'hundred ';sk=1;} if ((x-i)%3==1) {if (sk) str += th[(x-i-1)/3] + ' ';sk=0;}} if (x != s.length) {var y = s.length; str += 'rupees and '; for (var i=x+1; i<y; i++) str += dg[n[i]] +' ';} return str.replace(/\s+/g,' ');}
+	let dg = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+	var tn = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen',
+	'nineteen'];
+	var tw = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+
+	function toWords(s) {
+		s = s.toString();
+		s = s.replace(/[\, ]/g, '');
+		if (s != parseFloat(s)) return 'not a number';
+		var x = s.indexOf('.');
+		if (x == -1) x = s.length;
+		if (x > 15) return 'too big';
+		var n = s.split('');
+		var str = '';
+		var sk = 0;
+		for (var i = 0; i < x; i++) {
+			if ((x - i) % 3 == 2) {
+				if (n[i] == '1') {
+					str += tn[Number(n[i + 1])] + ' ';
+					i++;
+					sk = 1;
+				} else if (n[i] != 0) {
+					str += tw[n[i] - 2] + ' ';
+					sk = 1;
+				}
+			} else if (n[i] != 0) {
+				str += dg[n[i]] + ' ';
+				if ((x - i) % 3 == 0) str += 'hundred ';
+				sk = 1;
+			}
+			if ((x - i) % 3 == 1) {
+				if (sk) str += th[(x - i - 1) / 3] + ' ';
+				sk = 0;
+			}
+		}
+		if (x != s.length) {
+			var y = s.length;
+			str += 'rupees and ';
+			for (var i = x + 1; i < y; i++) str += dg[n[i]] + ' ';
+		}
+		return str.replace(/\s+/g, ' ');
+	}
+
 </script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.js" ></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.js"></script>
 <script type="text/javascript">
-        $(document).on("click", ".invoice", function () {
-            $(this).css('display','none');
-			Convert_HTML_To_PDF();
-			
-        });
-    </script>
+	$(document).on("click", ".invoice", function () {
+		$(this).css('display', 'none');
+		Convert_HTML_To_PDF();
+
+	});
+
+</script>
 <script>
-function Convert_HTML_To_PDF() {
-	window.jsPDF = window.jspdf.jsPDF;
-    var doc = new jsPDF();
-    var elementHTML = document.querySelector("#invoiceholder");
-    doc.html(elementHTML, {
-    callback: function(doc) {
-        // Save the PDF
-        doc.save('sample-document.pdf');
-    },
-    x: 15,
-    y: 15,
-    width: 170, //target width in the PDF document
-    windowWidth: 850 //window width in CSS pixels
-});
-}
+	function Convert_HTML_To_PDF() {
+		window.jsPDF = window.jspdf.jsPDF;
+		var doc = new jsPDF();
+		var elementHTML = document.querySelector("#invoiceholder");
+		doc.html(elementHTML, {
+			callback: function (doc) {
+				// Save the PDF
+				doc.save('sample-document.pdf');
+			},
+			x: 15,
+			y: 15,
+			width: 170, //target width in the PDF document
+			windowWidth: 850 //window width in CSS pixels
+		});
+	}
+
 </script>
 
-     <script>
-        $(window).on("load", function() {
-            var main = $('#cBalance').text();
-            var disc = $('#chDiscount').text();
-            var dec = (disc / 100).toFixed(2); //its convert 10 into 0.10
-            var mult = main * dec; // gives the value for subtract from main value
-            var discont = main - mult;
-            $('#result').text(discont);
-            // alert(discont);
-        });
-    </script>
+<script>
+	$(window).on("load", function () {
+		var main = $('#cBalance').text();
+		var disc = $('#chDiscount').text();
+		var dec = (disc / 100).toFixed(2); //its convert 10 into 0.10
+		var mult = main * dec; // gives the value for subtract from main value
+		var discont = main - mult;
+		$('#result').text(discont);
+		// alert(discont);
+	});
+
+</script>
 
 
 </body>
